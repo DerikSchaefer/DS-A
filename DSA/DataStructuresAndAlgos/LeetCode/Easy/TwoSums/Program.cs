16d30981b2bd2f5356e61cadeaeaae129cb9c8b5
Derik Schaefer <72780230+DerikSchaefer@users.noreply.github.com>
2022-07-28 10:14:58 -0700

    Add files via upload

---
+++ DSA/DataStructuresAndAlgos/LeetCode/Easy/TwoSums/Program.cs
@@ -0,0 +1,23 @@
+﻿int target = 9;
+int[] nums = { 2, 7, 11, 15 };
+int[] result = new int[2];
+
+int[] TwoSum(int[] nums, int target)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for (int j = i + 1; j < nums.Length; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                result[0] = i;
+                result[1] = j;
+            }
+        }
+    }
+    return result;
+}
+
+TwoSum(nums, target);
+Console.WriteLine("Results: {0} {1}", result[0], result[1]);
+Console.ReadLine();
