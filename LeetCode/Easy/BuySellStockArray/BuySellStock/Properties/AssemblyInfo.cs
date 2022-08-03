cf19e889226f7f8722a671cfc0c03716385f0af6
Derik Schaefer <72780230+DerikSchaefer@users.noreply.github.com>
2022-08-02 21:31:11 -0700

    Add files via upload

---
+++ LeetCode/Easy/BuySellStockArray/BuySellStock/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BuySellStock")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BuySellStock")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("39f7d348-8e1a-4e8f-9380-a1f117cdd49a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LeetCode/Easy/BuySellStockArray/BuySellStock2/StockArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuySellStock2
+{
+    internal class StockArray
+    {
+
+        public int MaxProfit(int[] prices)
+        {
+            {
+
+                if (prices == null || prices.Length == 0)
+                {
+                    return 0;
+                }
+
+                int profit = 0;
+
+                for (int i = 0; i < prices.Length - 1; i++)
+                {
+                    if (prices[i + 1] > prices[i])
+                    {
+                        profit += (prices[i + 1] - prices[i]);
+                    }
+                }
+                Console.WriteLine($"Your profit is {profit}");
+                Console.ReadLine();
+                return profit;
+            }
+        }
+    }
+}
